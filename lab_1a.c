5cc0489987388a23169e3f4490db7e80639bddf0
Gary Tuttle <gtuttle@iastate.edu>
2018-01-22 18:10:16 -0600

    Create lab_1a.c

---
+++ lab_1a.c
@@ -0,0 +1,15 @@
+//Lab 1a
+Jan 22 - Jan 26
+
+#include <stdio>
+
+int main()
+
+    printf("Woo Hoo! EE 285, Lab 1!!\n\n")
+    
+    printf("8:00 a.m. labs are no fun.");
+    
+    printf( Hopefully, this lab is not too difficult.\n\n");
+           
+    return 0;
+}
